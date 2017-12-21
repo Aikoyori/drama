d5adc5be321c5206634c2b4f257366afb5d3a3aa
zi8tx <sesoui72@gmail.com>
2017-12-21 21:50:28 +0100

    maincra cs port

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 #### fuck
 ##### end
 ###### me
+####### now
 This is a repository about what would be happen In the future :^)
---
+++ maincra.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading;
+
+namespace maincra
+{
+  public class maincra
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("u");
+      Thread.Sleep(1000);
+      Console.WriteLine("gei");
+    }
+  }
+}
