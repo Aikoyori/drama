17af74c908b9fb06845d5f9e691959d03c4a66d8
TheFox <krakaka088@gmail.com>
2018-11-10 17:08:47 +0000

    simplify

--- maincra.js
+++ maincra.js
@@ -1,17 +1,4 @@
-function sleep(ms) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > ms) {
-            break;
-        }
-    }
-}
-
-function maincra() {
-    console.log(new Date());
-    console.log('u');
-    sleep(6000);
-    console.log('gey!');
-}
-
-maincra();
+console.log("u")
+setTimeout(() => {
+    console.log("gey")
+}, 60000)
